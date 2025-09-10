06b9ba4dcc88257333d9fcf7959339d8c60d81a1
Andreas Graubach <andreasgraubach@MacBook-Pro-de-Andreas.local>
2025-09-10 17:35:41 -0300

    multiple instances

---
+++ Basics/createInstance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "FirstInstance" {
+    count = 3
+    ami = "Enter AMI ID"
+    instance_type = "Enter instance type" # Ex.: t2.micro
+
+    tags = {
+        Name = "threedemoinstance"
+    }
+}
